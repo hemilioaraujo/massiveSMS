c4c5d2ba193a6106ac2b1ed1fa3792e99a3df178
HLAM <hemilioaraujo@gmail.com>
2020-04-04 19:55:37 -0300

    visual inicial definido

---
+++ app/src/main/java/com/hemilioaraujo/massivesms/MainActivity.java
@@ -0,0 +1,18 @@
+package com.hemilioaraujo.massivesms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    final int SEND_SMS_PERMISSION_REQUEST_CODE = 1;
+
+    EditText
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
