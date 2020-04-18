0d75823592ed4198eddba3b3e74eff84a99d03ef
HLAM <hemilioaraujo@gmail.com>
2020-04-18 15:52:54 -0300

    alterado o campo de mensagem para Multiline Text

--- app/src/main/java/com/hemilioaraujo/massivesms/MainActivity.java
+++ app/src/main/java/com/hemilioaraujo/massivesms/MainActivity.java
@@ -1,18 +1,59 @@
 package com.hemilioaraujo.massivesms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button send;
+    EditText number, message;
 
-    final int SEND_SMS_PERMISSION_REQUEST_CODE = 1;
-
-    EditText
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        send = (Button)findViewById(R.id.btnSend);
+        number = findViewById(R.id.txtNumber);
+        message = findViewById(R.id.txtMessage);
+
+        if(ActivityCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED&& ActivityCompat.checkSelfPermission(
+                MainActivity.this,Manifest
+                        .permission.SEND_SMS)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]
+                    { Manifest.permission.SEND_SMS,},1000);
+        }else{
+
+        };
+
+        send.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendMessage(number.getText().toString(),message.getText().toString());
+            }
+        });
+    }
+
+    private void sendMessage (String number, String message){
+        try {
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(number,null,message,null,null);
+            Toast.makeText(getApplicationContext(), "Mensaje Enviado.", Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Mensaje no enviado, datos incorrectos.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
     }
 }
