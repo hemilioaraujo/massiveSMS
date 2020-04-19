773c9719e1a6b5d1db3098acfc52394531b2df7f
HLAM <hemilioaraujo@gmail.com>
2020-04-19 19:24:12 -0300

    adicionada função de acessar a agenda ao botão de pesquisa. Ao selecionar contato o número é escrito no campo number.

--- app/src/main/java/com/hemilioaraujo/massivesms/MainActivity.java
+++ app/src/main/java/com/hemilioaraujo/massivesms/MainActivity.java
@@ -5,8 +5,13 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +20,7 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button send;
+    Button send, search;
     EditText number, message;
 
     @Override
@@ -26,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
         send = (Button)findViewById(R.id.btnSend);
         number = findViewById(R.id.txtNumber);
         message = findViewById(R.id.txtMessage);
+        search = findViewById(R.id.btnSearch);
 
+        //        CONFIRMA PERMISSÃO PARA ENVIAR SMS
         if(ActivityCompat.checkSelfPermission(
                 MainActivity.this, Manifest.permission.SEND_SMS)
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
@@ -38,9 +45,27 @@ public class MainActivity extends AppCompatActivity {
 
         };
 
+        //        CONFIRMA PERMISSÃO PARA ACESSAR CONTATOS
+        if (ActivityCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                    1);
+        } else {
+
+        }
+
         send.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 sendMessage(number.getText().toString(),message.getText().toString());
+            }
+        });
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, 1);
             }
         });
     }
@@ -57,5 +82,50 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            Uri contactData = data.getData();
+            Cursor c = getContentResolver().query(contactData, null, null, null, null);
+            if (c.moveToFirst()) {
 
+                String phoneNumber = "", emailAddress = "";
+                String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                String contactId = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
+                //http://stackoverflow.com/questions/866769/how-to-call-android-contacts-list   our upvoted answer
+
+                String hasPhone = c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+                if (hasPhone.equalsIgnoreCase("1"))
+                    hasPhone = "true";
+                else
+                    hasPhone = "false";
+
+                if (Boolean.parseBoolean(hasPhone)) {
+                    Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
+                    while (phones.moveToNext()) {
+                        phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    }
+                    phones.close();
+                }
+
+                // Find Email Addresses
+                Cursor emails = getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + contactId, null, null);
+                while (emails.moveToNext()) {
+                    emailAddress = emails.getString(emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                }
+                emails.close();
+
+                //mainActivity.onBackPressed();
+                // Toast.makeText(mainactivity, "go go go", Toast.LENGTH_SHORT).show();
+
+//                tvname.setText("Name: " + name);
+                number.setText(phoneNumber);
+//                tvmail.setText("Email: " + emailAddress);
+//                Log.d("curs", name + " num" + phoneNumber + " " + "mail" + emailAddress);
+            }
+            c.close();
+        }
+    }
 }
